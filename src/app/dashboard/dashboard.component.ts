d5b0617ed4a5875ab810e79989fa3e79a1e6e50e
FranciscoErick0135 <193241@ids.upchiapas.edu.mx>
2020-12-05 19:10:57 -0600

    table implementation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,26 +2,7 @@ import { CursorError } from '@angular/compiler/src/ml_parser/lexer';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthServiceService } from '../service/auth/auth-service.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -35,15 +16,21 @@ export class DashboardComponent implements OnInit {
   AgregarFormGroup : FormGroup;
   ModificarFormGroup : FormGroup;
   EliminarFormGroup : FormGroup;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
   bandera = 1;
+  usuarios = [];
+  info = "No hay datos"
 
-  constructor(private _formBuilder:FormBuilder, private _authServiceService : AuthServiceService) { }
+  constructor(private _formBuilder:FormBuilder, private _authServiceService : AuthServiceService, private _router: Router) { }
 
   
 
   ngOnInit(): void {
+
+    this._authServiceService.get().subscribe((data : any []) =>{
+      console.log(data);
+      this.usuarios = data;
+    });
+
     this.AgregarFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
       age: ['', Validators.required],
@@ -74,15 +61,11 @@ export class DashboardComponent implements OnInit {
     this.bandera=3
   }
 
-  aceptar(){
-    
-  }
 
   agregar(){
     const data = this.AgregarFormGroup.value;
     data.age = Number(data.age);
     if(data.name && data.age && data.e_mail){
-      console.log("excelente")
       this._authServiceService.post(data.name, data.age, data.e_mail).subscribe(access => {
         console.log(access)
       },error => {
@@ -90,6 +73,8 @@ export class DashboardComponent implements OnInit {
       }
       );
     }
+    
+      window.location.href = 'http://localhost:4200/dashboard';
   }
 
   modificar(){
@@ -104,6 +89,7 @@ export class DashboardComponent implements OnInit {
       }
       );
     }
+    window.location.href = 'http://localhost:4200/dashboard';
   }
 
   borrar(){
@@ -117,7 +103,7 @@ export class DashboardComponent implements OnInit {
       }
       );
     }
-
+    window.location.href = 'http://localhost:4200/dashboard';
   }
   
 }
