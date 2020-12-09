ba9f28fcc99b8e78fc3f1ba78cb214c8f99b6829
FranciscoErick0135 <193241@ids.upchiapas.edu.mx>
2020-12-09 02:08:27 -0600

    Register method

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FireauthService } from '../fireauth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from '../service/auth/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +12,35 @@ import { FireauthService } from '../fireauth.service';
   providers : [AngularFireAuth, FireauthService]
 })
 export class RegisterComponent implements OnInit {
+  registerFormGroup : FormGroup;
 
-  constructor(private fire:FireauthService) { }
+  constructor(private _formBuilder:FormBuilder, private fire:FireauthService, private _authServiceService : AuthServiceService, private _router: Router) { }
 
   ngOnInit(): void {
+    this.registerFormGroup = this._formBuilder.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
-  register(email : string, password : string){
-    this.fire.register(email,password);
-  }
+  register(){
+    const data = this.registerFormGroup.value;
+    if(data.user && data.first_name && data.last_name && data.email && data.password){
+      this._authServiceService.register(data.user, data.first_name, data.last_name, data.email, data.password).subscribe(access => {
+        console.log(data.user, data.password, data.first_name, data.last_name, data.email);
+        localStorage.setItem('user', JSON.stringify(access));
+        this._router.navigate(['login']);
+
+      },error => {
+        
+      }
+      );
+    }
+  } 
 
   registerGoogle(){
     this.fire.signInWithGoogle();
   }
+
 
 }
